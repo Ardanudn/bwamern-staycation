c8d8cb2a190a2bb3bd8296b94e25d86079e3058c
Ardanudhuhrinugroho <Ardanudhuhrinugroho@users.noreply.github.com>
2020-05-15 10:23:12 +0700

    finish until design to react

--- src/pages/DetailsPage.js
+++ src/pages/DetailsPage.js
@@ -1,14 +1,14 @@
 import React, { Component } from "react";
-
+import Fade from "react-reveal/Fade";
 
 import Header from "parts/Header";
 import PageDetailTitle from "parts/PageDetailTitle";
 import FeaturedImage from "parts/FeaturedImage";
-//import PageDetailDescription from "parts/PageDetailDescription";
-//import BookingForm from "parts/BookingForm";
-//import Activities from "parts/Activities";
-//import Testimony from "parts/Testimony";
-//import Footer from "parts/Footer";
+import PageDetailDescription from "parts/PageDetailDescription";
+import BookingForm from "parts/BookingForm";
+import Categories from "parts/Categories";
+import Testimony from "parts/Testimony";
+import Footer from "parts/Footer";
 
 import ItemDetails from 'json/itemDetails.json';
 
@@ -31,8 +31,28 @@ export default class DetailsPage extends Component {
           breadcrumb={breadcrumb} 
           data={ItemDetails} />
         <FeaturedImage data={ItemDetails.imageUrls}/>
-        
-       
+        <section className="container">
+          <div className="row">
+            <div className="col-7 pr-5">
+              <Fade bottom>
+                <PageDetailDescription data={ItemDetails} />
+              </Fade>
+            </div>
+            <div className="col-5">
+              <Fade bottom>
+                <BookingForm
+                  itemDetails={ItemDetails}
+                  
+                />
+              </Fade>
+            </div>
+          </div>
+        </section>
+
+        <Categories data={ItemDetails.categories} />
+        <Testimony data={ItemDetails.testimonial} />
+
+        <Footer />
       </>
     );
   }
